e4074f05f6f611829ce24ce3894c52a10e137a23
Roger Lemos <rogerlemos28@gmail.com>
2021-08-28 10:18:57 -0300

    Cria desáfio de par ou impar no nível básico

---
+++ desafios/engitnhando/par_ou_impar.py
@@ -0,0 +1,23 @@
+ """
+    Nivel fundamental
+    
+    Desafio: O programa deverá ler um número inteiro do usuario e retornar uma mensagem dizendo se o número é Par ou Impar.
+    A equipe define em qual condição a função par_impar() deverá retornar True ou False alinhando a estrutura de repetição a este retorno para imprimir a mensagem correta ou usuario.
+
+    Bonus: Escreva testes para seu código ,trate exceções e capriche nas mensagens ao usuario.
+
+    Compartilhe sua solução com a cumunidade deixando o link no comentário desde desáfio.
+ """ 
+num = int(input())
+
+def par_impar(num):
+    pass   #Apague está linha e complete o código
+
+if par_impar(num) == False:
+    pass
+elif par_impar(num) == True:
+    pass
+
+
+
+ 
